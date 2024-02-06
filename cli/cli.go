7763ee993e15625328db56e1342614a3ab11ba9c
Morpheus0x <morpheus0x@pm.me>
2024-02-06 22:45:26 +0100

    first draft: cli input parsing

--- cli/cli.go
+++ cli/cli.go
@@ -1,1 +1,55 @@
 package cli
+
+import (
+	"fmt"
+
+	h "github.com/KiloHost/apibase/helper"
+)
+
+// Basic Yes/No Dialog
+type Confirm struct {
+	// Default value is false, meaning no
+	Default h.ConfirmDefault
+	// How strict the input parsing should be
+	Sensitivity h.ConfirmSensitivity
+	// Prompt Text
+	Prompt string
+}
+
+// returns the string "yes" or "no" depending on user input, on error undefined
+func (c *Confirm) Ask() (string, error) {
+	out, err := c.askConfirm()
+	if err != nil {
+		return "", err
+	}
+	if out {
+		return "yes", nil
+	}
+	return "no", nil
+}
+
+// The returned bool is always true for yes and false for no, independent of the set default response
+func (c *Confirm) AskBool() (bool, error) {
+	return c.askConfirm()
+}
+
+// Internal ask method
+func (c *Confirm) askConfirm() (bool, error) {
+	q := c.Prompt
+	if c.Prompt == "" {
+		q = "Confirm?"
+	}
+	// TODO: cont
+	return false, fmt.Errorf(q)
+}
+
+type Input struct {
+	Default       string
+	RequiredChars uint
+	Prompt        string
+}
+
+func (i *Input) Get() (string, error) {
+	// TODO: cont
+	return "", fmt.Errorf("")
+}
---
+++ helper/helper.go
@@ -0,0 +1,19 @@
+package apibase
+
+type ConfirmDefault bool
+
+const (
+	No  ConfirmDefault = false
+	Yes ConfirmDefault = true
+)
+
+type ConfirmSensitivity uint
+
+const (
+	// Accepting any ordered subset of case insensitive letters of "yes" and "no", also enter for default
+	Patient ConfirmSensitivity = iota
+	// Requiring a fully typed out case insensitive "yes", "no" or "\n", otherwise ask repeatedly
+	Picky
+	// Requiring a fully typed out case insensitive "yes", otherwise immediately quit
+	Strict
+)
