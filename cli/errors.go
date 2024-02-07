5f13c40c3c7f528cfccec5a870b8ff44341f084a
Morpheus0x <morpheus0x@pm.me>
2024-02-07 16:46:44 +0100

    impl text input

--- cli/errors.go
+++ cli/errors.go
@@ -6,4 +6,5 @@ var (
 	ERR_READLINE        = errors.New("unable to create line reader")
 	ERR_SIGINT_RECEIVED = errors.New("sigint received")
 	ERR_INVALID_INPUT   = errors.New("no valid input specified")
+	ERR_MIN_INPUT_LEN   = errors.New("longer input required")
 )
--- cli/input.go
+++ cli/input.go
@@ -1,14 +1,67 @@
 package cli
 
-import "fmt"
+import (
+	"github.com/chzyer/readline"
+)
 
+// Input Dialog
 type Input struct {
-	Default       string
+	// default input to use when only pressing enter, shown in parenthesis after the prompt, if set
+	Default string
+	// minimum number of required characters
 	RequiredChars uint
-	Prompt        string
+	// if kept as default false, the prompt is repeated until a valid anser is provided, otherwise if true, only asking once
+	OnlyOnce bool
+	// Prompt Text, colon with single trailing space will be appended as separator to user input
+	Prompt string
 }
 
 func (i Input) Get() (string, error) {
-	// TODO: cont
-	return "", fmt.Errorf("")
+	return i.getInternal()
+}
+
+func (i Input) GetEmptyOnErr() string {
+	out, err := i.getInternal()
+	if err != nil {
+		return ""
+	}
+	return out
+}
+
+func (i Input) getInternal() (string, error) {
+	defaultValue := ""
+	if i.Default != "" {
+		defaultValue = i.Default
+	}
+	rl, err := readline.New(i.Prompt + defaultValue + ": ")
+	if err != nil {
+		return "", ERR_READLINE
+	}
+	defer rl.Close()
+
+	for {
+		line, err := rl.Readline()
+		if err != nil { // io.EOF, e.g. SIGINT
+			return "", ERR_SIGINT_RECEIVED
+		}
+		if len(line) == 0 {
+			if len(i.Default) >= int(i.RequiredChars) {
+				return i.Default, nil
+			} else {
+				if i.OnlyOnce {
+					return "", ERR_MIN_INPUT_LEN
+				} else {
+					continue
+				}
+			}
+		}
+		if len(line) < int(i.RequiredChars) {
+			if i.OnlyOnce {
+				return "", ERR_MIN_INPUT_LEN
+			} else {
+				continue
+			}
+		}
+		return line, nil
+	}
 }
