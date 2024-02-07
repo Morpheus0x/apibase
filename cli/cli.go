18b4020391010bd4f73f3f82d48f457be37c4d0c
Morpheus0x <morpheus0x@pm.me>
2024-02-07 14:10:56 +0100

    implemented Confirmation dialog

--- cli/cli.go
+++ cli/cli.go
@@ -2,16 +2,17 @@ package cli
 
 import (
 	"fmt"
+	"strings"
 
-	h "github.com/KiloHost/apibase/helper"
+	"github.com/chzyer/readline"
 )
 
 // Basic Yes/No Dialog
 type Confirm struct {
 	// Default value is false, meaning no
-	Default h.ConfirmDefault
-	// How strict the input parsing should be
-	Sensitivity h.ConfirmSensitivity
+	Default bool
+	// If kept as default false, the prompt is repeated until a valid anser is provided, otherwise if true, only asking once
+	OnlyOnce bool
 	// Prompt Text
 	Prompt string
 }
@@ -33,14 +34,60 @@ func (c Confirm) AskBool() (bool, error) {
 	return c.askConfirm()
 }
 
-// Internal ask method
-func (c Confirm) askConfirm() (bool, error) {
-	q := c.Prompt
-	if c.Prompt == "" {
-		q = "Confirm?"
+func (c Confirm) AskBoolDefaultOnErr() bool {
+	out, err := c.askConfirm()
+	if err != nil {
+		return c.Default
 	}
-	// TODO: cont
-	return false, fmt.Errorf(q)
+	return out
+}
+
+func (c Confirm) AskBoolFalseOnErr() bool {
+	out, err := c.askConfirm()
+	if err != nil {
+		return false
+	}
+	return out
+}
+
+// Internal ask method, panics
+func (c Confirm) askConfirm() (bool, error) {
+	prompt := c.Prompt
+	if c.Prompt == "" {
+		prompt = "Confirm?"
+	}
+	question := " [y/N]: "
+	if c.Default {
+		question = " [Y/n]: "
+	}
+
+	rl, err := readline.New(prompt + question)
+	if err != nil {
+		return false, err
+	}
+	defer rl.Close()
+
+	for {
+		line, err := rl.Readline()
+		if err != nil { // io.EOF
+			// return false, fmt.Errorf("^C received")
+			panic(fmt.Errorf("^C received"))
+		}
+		if len(line) == 0 {
+			return c.Default, nil
+		}
+
+		input := strings.ToLower(line)
+		if input == "y" || input == "ye" || input == "yes" {
+			return true, nil
+		}
+		if input == "n" || input == "no" {
+			return false, nil
+		}
+		if c.OnlyOnce {
+			return false, fmt.Errorf("no valid input specified")
+		}
+	}
 }
 
 type Input struct {
