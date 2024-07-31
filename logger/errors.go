555bea62ce2f83f0697b26f0a98126ad92527825
Morpheus0x <morpheus0x@pm.me>
2024-07-31 12:51:12 +0200

    rename package logger to log

--- logger/errors.go
+++
@@ -1,6 +0,0 @@
-package logger
-
-// TODO:
-// built in custom error types
-// method for registering new error handlers that determine severity
-// a passed function can be used to parse the custom error type for severity ...
