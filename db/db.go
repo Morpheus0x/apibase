c62837197086f8d81bcc973ad641a1ad5b85022e
Morpheus0x <morpheus0x@pm.me>
2024-08-09 08:43:25 +0200

    add Gorm and generic DB query

--- db/db.go
+++ db/db.go
@@ -1,6 +1,12 @@
 package db
 
-import "gopkg.cc/apibase/sqlite"
+import (
+	"fmt"
+	"reflect"
+
+	"gopkg.cc/apibase/sqlite"
+	"gorm.io/gorm"
+)
 
 // Generic db driver for package web
 
@@ -11,8 +17,46 @@ const (
 	PostgreSQL
 )
 
+type DBAdapter uint
+
+const (
+	Apibase DBAdapter = iota
+	Gorm
+	Pgx
+)
+
 type DB struct {
 	Kind     DBKind
+	Adapter  DBAdapter
 	SQLite   *sqlite.SQLite
-	Postgres string // TODO: new
+	Postgres any
+}
+
+func RawQuery[T any](db *DB, query string) ([]*T, error) {
+	switch db.Kind {
+	case SQLite:
+		if db.Adapter != Apibase {
+			return []*T{}, fmt.Errorf("invalid sqlite adapter")
+		}
+		return sqlite.RawQuery[T](db.SQLite, query)
+	case PostgreSQL:
+		if db.Adapter != Gorm {
+			return []*T{}, fmt.Errorf("only gorm is implemented for postgres")
+		}
+		if reflect.TypeOf(db.Postgres) != reflect.TypeOf(&gorm.DB{}) {
+			return []*T{}, fmt.Errorf("db.Postgres must be valid gorm adapter")
+		}
+		model := []T{}
+		tx := db.Postgres.(*gorm.DB).Model(&model).Raw(query)
+		if tx.Error != nil {
+			return []*T{}, fmt.Errorf("error running raw postgres query: %v", tx.Error)
+		}
+		out := []*T{}
+		for _, t := range model {
+			out = append(out, &t)
+		}
+		return out, nil
+	default:
+		return []*T{}, fmt.Errorf("no valid DB specified")
+	}
 }
