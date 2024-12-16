54ff57a87b82fddc05a40e78ead403e3c3aef46b
Morpheus0x <morpheus0x@pm.me>
2024-12-16 13:05:30 +0100

    add cobra cli arg parsing for common config path, version, verbose and help

---
+++ cmd/cmd.go
@@ -0,0 +1,94 @@
+package cmd
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/spf13/cobra"
+	"gopkg.cc/apibase/log"
+)
+
+type CmdConfig struct {
+	AppName           string
+	ShortDescr        string
+	LongDescr         string
+	Version           string
+	DefaultConfigPath string
+	PrintFuct         PrintFuct
+}
+
+type PrintFuct func()
+
+var defaultVersionPrint = func() {
+	fmt.Printf("%s v%s\n", appConfig.AppName, appConfig.Version)
+}
+
+type Settings struct {
+	ConfigFile  string
+	Verbose     bool
+	VeryVerbose bool
+	Help        bool
+}
+
+func (s Settings) GetLogLevel() log.Level {
+	if s.VeryVerbose {
+		return log.LevelDebug
+	}
+	if s.Verbose {
+		return log.LevelInfo
+	}
+	return log.LevelNotice
+}
+
+var (
+	appConfig   CmdConfig
+	appSettings Settings
+	stopExec    bool = false
+)
+
+func ConfigureCLI(conf CmdConfig) *cobra.Command {
+	appConfig = conf
+	if reflect.ValueOf(conf.PrintFuct).IsZero() {
+		appConfig.PrintFuct = defaultVersionPrint
+	}
+	return &cobra.Command{
+		Use:   appConfig.AppName,
+		Short: appConfig.ShortDescr,
+		Long:  appConfig.LongDescr,
+		Run: func(cmd *cobra.Command, args []string) {
+			if cmd.Flag("version").Value.String() == "true" {
+				appConfig.PrintFuct()
+				stopExec = true
+			}
+		},
+	}
+}
+
+// Parse cli arguments, returns true if program should exit
+func Execute(root *cobra.Command) (Settings, bool) {
+	defaultHelpFunc := root.HelpFunc()
+
+	root.PersistentFlags().StringVarP(&appSettings.ConfigFile, "config", "c", appConfig.DefaultConfigPath, "config file")
+	root.PersistentFlags().Bool("version", false, "print version, license and additional software info")
+	root.PersistentFlags().BoolVarP(&appSettings.Verbose, "verbose", "v", false, "more detailed output, log level: info")
+	root.PersistentFlags().BoolVar(&appSettings.VeryVerbose, "very-verbose", false, "even more detailed output, log level: debug")
+
+	root.SetHelpFunc(func(cmd *cobra.Command, args []string) {
+		defaultHelpFunc(cmd, args)
+		stopExec = true
+	})
+
+	root.AddCommand(&cobra.Command{
+		Use:   "version",
+		Short: "print version, license and additional software info",
+		Run: func(cmd *cobra.Command, args []string) {
+			appConfig.PrintFuct()
+			stopExec = true
+		},
+	})
+	err := root.Execute()
+	if err != nil {
+		return appSettings, true
+	}
+	return appSettings, stopExec
+}
--- go.mod
+++ go.mod
@@ -25,10 +25,12 @@ require (
 	github.com/gorilla/mux v1.6.2 // indirect
 	github.com/gorilla/securecookie v1.1.1 // indirect
 	github.com/gorilla/sessions v1.1.1 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
 	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/stytchauth/squirrel v1.5.3-0.20230822204145-fbce445169d2 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
@@ -41,6 +43,7 @@ require (
 
 require (
 	github.com/mattn/go-sqlite3 v1.14.22
+	github.com/spf13/cobra v1.8.1
 	github.com/stytchauth/sqx v0.6.0
 	golang.org/x/crypto v0.30.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
