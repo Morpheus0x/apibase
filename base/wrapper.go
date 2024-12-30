1509bed50d922aa1db3c0ea217c752928dec4ebc
Morpheus0x <morpheus0x@pm.me>
2024-12-30 13:17:54 +0100

    move channel cleanup to own function, change CloseChain array to prepend new channels in order to cleanup in reverse order

--- base/wrapper.go
+++ base/wrapper.go
@@ -8,17 +8,17 @@ import (
 )
 
 // used to add a stage in the close channel chain, returns offset of channel to listen to,
-// guarantees offset to be second to last element of chain
+// will always be 0, since cleanup is done in reverse order
 func (apiBase *ApiBase[T]) registerCloseStage() uint {
-	var offset uint
 	if len(apiBase.CloseChain) < 1 {
+		// if CloseChain is an empty array, also create the "next" channel
 		apiBase.CloseChain = append(apiBase.CloseChain, make(chan struct{}))
-		offset = 0
-	} else {
-		offset = uint(len(apiBase.CloseChain) - 1)
 	}
-	apiBase.CloseChain = append(apiBase.CloseChain, make(chan struct{}))
-	return offset
+	apiBase.CloseChain = append([]chan struct{}{make(chan struct{})}, apiBase.CloseChain...)
+	// since the close chain array should cleanup everything in reverse order,
+	// the new close channel will always be prepended.
+	// Therefore this function always returns index 0
+	return 0 // TODO: maybe don't return index at all
 }
 
 // returns shutdown and next channels which can be used for own application go routines
@@ -40,17 +40,33 @@ func (apiBase *ApiBase[T]) StartRest(api *webtype.ApiServer) *log.Error {
 
 	err := web.StartRest(api, apiBase.ApiConfig.ApiBind, apiBase.CloseChain[i], apiBase.CloseChain[i+1])
 	if !err.IsNil() {
-		// cleanup channel close chain array
-		if len(apiBase.CloseChain) == 2 {
-			// if this is the first entry in the channel close chain remove both shutdown and next channels by clearing the array
-			apiBase.CloseChain = nil
-		} else if len(apiBase.CloseChain) > 2 {
-			// remove the last channel, therefore the new last element will be the "next" channel used to close subsequent go routines
-			apiBase.CloseChain = apiBase.CloseChain[:len(apiBase.CloseChain)-1]
-		} else {
-			log.Log(log.LevelCritical, "close chain array only contains one channel, this should not happen!")
-		}
+		apiBase.startupErrorCleanup()
 		return err
 	}
 	return log.ErrorNil()
+}
+
+// cleanup channel close chain array
+func (apiBase *ApiBase[T]) startupErrorCleanup() {
+	ccLength := len(apiBase.CloseChain)
+	if ccLength < 1 {
+		// nothing to clean up
+		return
+	}
+	if ccLength < 2 {
+		log.Log(log.LevelCritical, "close chain array only contains one channel, this should not happen!")
+		return
+	}
+	if ccLength == 2 {
+		// if this is the first entry in the channel close chain, close both shutdown and next channels and clear the array
+		// all channel listeners should have been cleaned up by the setup function on error
+		close(apiBase.CloseChain[0])
+		close(apiBase.CloseChain[1])
+		apiBase.CloseChain = nil
+		return
+	}
+	// ccLength > 2
+	// close and remove the last channel, therefore the new last element will be the "next" channel used to close subsequent go routines
+	close(apiBase.CloseChain[ccLength-1])
+	apiBase.CloseChain = apiBase.CloseChain[:ccLength-1]
 }
