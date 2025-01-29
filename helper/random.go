76e1a77f5adf1509b26934e49136edda0d390603
Morpheus0x <morpheus0x@pm.me>
2025-01-29 13:08:08 +0100

    add url safe base64 random string generator

--- helper/random.go
+++ helper/random.go
@@ -1,6 +1,9 @@
 package helper
 
 import (
+	cr "crypto/rand"
+	"encoding/base64"
+	"fmt"
 	"math/rand/v2"
 )
 
@@ -12,4 +15,17 @@ func RandomString(n int) string {
 		b[i] = randDict[rand.IntN(len(randDict))]
 	}
 	return string(b)
+}
+
+// Create a random URL safe base64 string, n specifies amount of bytes
+func RandomBase64(n uint) string {
+	if n == 0 {
+		return ""
+	}
+	token := make([]byte, n)
+	_, err := cr.Read(token)
+	if err != nil {
+		panic(fmt.Sprintf("RandomBase64 crypto.rand.Read() has error: %v. This should never happen!", err))
+	}
+	return base64.URLEncoding.EncodeToString(token)
 }
