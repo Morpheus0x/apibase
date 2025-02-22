7c486f56f10d0f280b821ac40f0f735fac2d6925
Morpheus0x <morpheus0x@pm.me>
2025-02-22 15:33:47 +0100

    impl cli select

--- cli/errors.go
+++ cli/errors.go
@@ -7,4 +7,5 @@ var (
 	ERR_SIGINT_RECEIVED = errors.New("sigint received")
 	ERR_INVALID_INPUT   = errors.New("no valid input specified")
 	ERR_MIN_INPUT_LEN   = errors.New("longer input required")
+	ERR_READ_BYTE       = errors.New("unabel to read input byte")
 )
