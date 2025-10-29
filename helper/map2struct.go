15f2de6e19c47c2037d269b2e3eb74975db33201
Morpheus0x <morpheus0x@pm.me>
2025-10-29 09:38:09 +0100

    make map to struct recursive to resolve nested maps/structs

--- helper/map2struct.go
+++ helper/map2struct.go
@@ -6,10 +6,11 @@ import (
 	"slices"
 )
 
-// data must be of type map[string]any, target must be a pointer to the desired output struct
+// data must be of type map[string]any,
+// target must be a pointer to the desired output struct,
+// the target struct must have tag `mapkey` corresponding to the map key being parsed
 func MapToStruct(data any, target any) []error {
 	targetValue := reflect.ValueOf(target).Elem() // Dereference pointer to get actual struct
-	targetType := targetValue.Type()              // Get Struct Type
 
 	realData, ok := data.(map[string]any)
 	if !ok {
@@ -17,30 +18,38 @@ func MapToStruct(data any, target any) []error {
 	}
 
 	mapErrors := []error{}
+	internalMapToStruct(realData, targetValue, &mapErrors, "", "")
+	return mapErrors
+}
+
+func internalMapToStruct(data map[string]any, target reflect.Value, mapErrors *[]error, parentStructField string, parentMapKey string) {
+	targetType := target.Type() // Get Struct Type
 	parsedMapKeys := []string{}
 
-	for i := 0; i < targetValue.NumField(); i++ {
+	for i := 0; i < target.NumField(); i++ {
 		fieldType := targetType.Field(i)
 
 		// get map value according to struct field tag
 		key := fieldType.Tag.Get("mapkey")
 		if key == "" {
-			mapErrors = append(mapErrors, fmt.Errorf(
-				"target struct is missing tag 'mapkey' for field '%s'",
+			*mapErrors = append(*mapErrors, fmt.Errorf(
+				"target struct is missing tag 'mapkey' for field '%s%s'",
+				parentStructField,
 				fieldType.Name,
 			))
 			continue
 		}
-		val, ok := realData[key]
+		val, ok := data[key]
 		if !ok {
-			mapErrors = append(mapErrors, fmt.Errorf("key '%s' doesn't exist in data map", key))
+			*mapErrors = append(*mapErrors, fmt.Errorf("key '%s%s' doesn't exist in data map", parentMapKey, key))
 			continue
 		}
 
 		// sanity check, that struct field can be set
-		if !targetValue.Field(i).CanSet() {
-			mapErrors = append(mapErrors, fmt.Errorf(
-				"unable to set struct field '%s' (mapkey: %s) with value from map",
+		if !target.Field(i).CanSet() {
+			*mapErrors = append(*mapErrors, fmt.Errorf(
+				"unable to set struct field '%s%s' (mapkey: %s) with value from map",
+				parentStructField,
 				fieldType.Name,
 				key,
 			))
@@ -49,12 +58,14 @@ func MapToStruct(data any, target any) []error {
 
 		// Check if map value is nil and set struct field accordingly
 		if val == nil { //  && fieldType.Type.Kind() == reflect.Ptr
-			targetValue.Field(i).Set(reflect.Zero(fieldType.Type))
+			target.Field(i).Set(reflect.Zero(fieldType.Type))
 			parsedMapKeys = append(parsedMapKeys, key)
 			if fieldType.Type.Kind() != reflect.Ptr {
-				mapErrors = append(mapErrors, fmt.Errorf(
-					"map value for key '%s' is nil, but struct field '%s' is of non ptr type '%s', was assigned zero value anyway",
+				*mapErrors = append(*mapErrors, fmt.Errorf(
+					"map value for key '%s%s' is nil, but struct field '%s%s' is of non ptr type '%s', was assigned zero value anyway",
+					parentMapKey,
 					key,
+					parentStructField,
 					fieldType.Name,
 					fieldType.Type.String(),
 				))
@@ -75,17 +86,20 @@ func MapToStruct(data any, target any) []error {
 			targetType = fieldType.Type.Elem()
 		}
 
-		// If map value isn't assignable to struct field, check if number conversion is needed
-		if !valReflect.Type().AssignableTo(fieldType.Type) {
+		// If map value isn't directly assignable to struct field, check for valid conversions
+		if !valReflect.Type().AssignableTo(targetType) { // TODO: check if AssignableTo(targetType) or AssignableTo(fieldType.Type) is correct
 			if valReflect.Type().ConvertibleTo(targetType) {
+				// Process any number to target struct field type
 				if validateConvertTypes(valReflect.Kind(), targetType.Kind()) {
 					valReflect = valReflect.Convert(targetType)
 				} else {
-					mapErrors = append(mapErrors, fmt.Errorf(
-						"map value of type '%s' for key '%s' is assignable to data type '%s' of struct field '%s' (target type %s), but no matching conversion found",
+					*mapErrors = append(*mapErrors, fmt.Errorf(
+						"map value of type '%s' for key '%s%s' is assignable to data type '%s' of struct field '%s%s' (target type %s), but no matching conversion found",
 						valReflect.Type().String(),
+						parentMapKey,
 						key,
 						targetType.String(),
+						parentStructField,
 						fieldType.Name,
 						targetType.Kind().String(),
 					))
@@ -93,7 +107,8 @@ func MapToStruct(data any, target any) []error {
 			} else if valReflect.Kind() == reflect.Slice &&
 				targetType.Kind() == reflect.Slice &&
 				targetType.Elem().Kind() == reflect.String {
-
+				// Process []string
+				// TODO: implement processing of any array of convertible values
 				valActual := valReflect.Interface().([]interface{})
 				res := make([]string, len(valActual))
 				for i, v := range valActual {
@@ -101,26 +116,39 @@ func MapToStruct(data any, target any) []error {
 				}
 				valReflect = reflect.ValueOf(res)
 			} else if targetType.Kind() == reflect.Struct {
-				mapErrors = append(mapErrors, fmt.Errorf("struct field '%s' is struct, map value is: %v", fieldType.Name, valReflect.Interface()))
-				parsedMapKeys = append(parsedMapKeys, key)
-				continue
-				// TODO: make this recursive
+				// Recursively process nested struct
+				valReflect = reflect.New(targetType).Elem()
+				internalMapToStruct(
+					valReflect.Interface().(map[string]interface{}),
+					valReflect,
+					mapErrors,
+					getParentFieldKey(fieldType.Name, parentStructField, -1),
+					getParentFieldKey(key, parentMapKey, -1),
+				)
 			} else if targetType.Kind() == reflect.Slice &&
 				targetType.Elem().Kind() == reflect.Struct {
-
+				// Recursively process nested []struct
 				valActual := valReflect.Interface().([]interface{})
-				for i, va := range valActual {
-					mapErrors = append(mapErrors, fmt.Errorf("struct field '%s' is struct array, map value[%d] is: %v", fieldType.Name, i, va.(map[string]interface{})))
-					// TODO: make this recursive
+				valReflect = reflect.MakeSlice(targetType, len(valActual), len(valActual))
+				for k, va := range valActual {
+					elemValReflect := reflect.New(targetType.Elem()).Elem()
+					internalMapToStruct(
+						va.(map[string]interface{}),
+						elemValReflect,
+						mapErrors,
+						getParentFieldKey(fieldType.Name, parentStructField, k),
+						getParentFieldKey(key, parentMapKey, k),
+					)
+					valReflect.Index(k).Set(elemValReflect)
 				}
-				parsedMapKeys = append(parsedMapKeys, key)
-				continue
 			} else {
-				mapErrors = append(mapErrors, fmt.Errorf(
-					"map value of type '%s' for key '%s' is not assignable to data type '%s' of struct field '%s', ",
+				*mapErrors = append(*mapErrors, fmt.Errorf(
+					"map value of type '%s' for key '%s%s' is not assignable to data type '%s' of struct field '%s%s', ",
 					valReflect.Type().String(),
+					parentMapKey,
 					key,
 					targetType.String(),
+					parentStructField,
 					fieldType.Name,
 				))
 				parsedMapKeys = append(parsedMapKeys, key)
@@ -136,29 +164,17 @@ func MapToStruct(data any, target any) []error {
 		}
 
 		// Set target struct field
-		targetValue.Field(i).Set(valReflect)
+		target.Field(i).Set(valReflect)
 		parsedMapKeys = append(parsedMapKeys, key)
 	}
 
 	// Check if any map keys were missed
-	for k := range realData {
+	for k := range data {
 		if !slices.Contains(parsedMapKeys, k) {
-			mapErrors = append(mapErrors, fmt.Errorf("data map contains key '%s' which isn't present in target struct", k))
+			*mapErrors = append(*mapErrors, fmt.Errorf("data map contains key '%s%s' which isn't present in target struct", parentMapKey, k))
 		}
 	}
-	return mapErrors
 }
-
-// TODO: move all this code to internal function which is called recursively on all child map[string]interface types
-// func internalMapToStruct(data map[string]any, target reflect.Value, mapErrors *[]error) {
-// 		// ...
-// 		val, ok := data[key]
-// 		if !ok {
-// 			*mapErrors = append(*mapErrors, fmt.Errorf("key '%s' doesn't exist in data map", key))
-// 			continue
-// 		}
-// 		// ...
-// }
 
 func validateConvertTypes(k reflect.Kind, l reflect.Kind) bool {
 	if k == reflect.String && l == reflect.String {
@@ -171,4 +187,16 @@ func validateConvertTypes(k reflect.Kind, l reflect.Kind) bool {
 		return true
 	}
 	return false
+}
+
+func getParentFieldKey(name string, parent string, i int) string {
+	index := ""
+	if i != -1 {
+		index = fmt.Sprintf("[%d]", i)
+	}
+	if parent == "" {
+		return fmt.Sprintf("%s%s.", name, index)
+	} else {
+		return fmt.Sprintf("%s%s%s.", parent, name, index)
+	}
 }
